c6e012135cd57f3bec6b8372606fdf7e739a6015
allykeightley <allysonkeightley@outlook.com>
2025-05-26 11:09:51 -0700

    feat: Test pre-commit hook

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,27 @@
-'use client'
+"use client";
 
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ThemeToggle } from "@/components/dashboard/ThemeToggle";
-import {AuroraText} from "@/components/magicui/aurora-text";
+import { AuroraText } from "@/components/magicui/aurora-text";
 import Link from "next/link";
 
-
 export default function Header() {
-    return (
-        <header className="py-6 px-4 border-b-1 border-gray-200">
-            <Link href="/"><h1 className="text-center font-bold tracking-tight text-2xl md:text-3xl lg:text-3xl">
-                Internship <AuroraText>tracker.</AuroraText>
-            </h1></Link>
-            <div className="flex items-end justify-end gap-x-5 mr-5">
-                <ThemeToggle />
-                <div className="flex">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="py-6 px-4 border-b-1 border-gray-200">
+      <Link href="/">
+        <h1 className="text-center font-bold tracking-tight text-2xl md:text-3xl lg:text-3xl">
+          Internship <AuroraText>tracker.</AuroraText>
+        </h1>
+      </Link>
+      <div className="flex items-end justify-end gap-x-5 mr-5">
+        <ThemeToggle />
+        <div className="flex">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+    </header>
+  );
 }
-
